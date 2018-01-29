2fe25b9b9cfc5e1fc2ca1e59271b2412023c77cf
Olaf Janssen <o.t.a.janssen@gmail.com>
2018-01-30 00:13:22 +0100

    KinectForEllai; refactored WebSockets stuff into a separate Handler class

--- src/KinectForEllai/KinectForEllai/Program.cs
+++ src/KinectForEllai/KinectForEllai/Program.cs
@@ -11,36 +11,22 @@ namespace KinectForEllai
 {
     class Program
     {
-        public static WebSocket WebSocket = null;
 
-        // Get defaut KinectSensor (we assume only 1 Kinect is connected)
-        private static readonly KinectSensor KinectSensor = KinectSensor.GetDefault();
-        
+        public static KinectHandler KinectHandler = null; 
+        public static WebSocketHandler WebSocketHandler = null;
+
         static void Main(string[] args)
         {
-            KinectHandler kinectHandler = new KinectHandler(BodyUpdated);
-            
-            // TODO the websockets connection is not stable, meaning the program cannot handle a server disconnect well, nor starting up without a running server
-            WebSocket = new WebSocket("ws://localhost:8080");
-            using (var ws = WebSocket)
-            {
-                ws.OnMessage += (sender, e) =>
-                    Console.WriteLine("Incoming message: " + e.Data);
-                ws.Connect();
+            KinectHandler = new KinectHandler(BodyUpdated);
+            WebSocketHandler = new WebSocketHandler("localhost:8080", "kinectforellai");
 
-                Console.ReadKey(true);
-            }
+            Console.ReadKey(true);
         }
 
         static void BodyUpdated(List<BodyMessage> bodyMessages)
         {
-            var message = new { Channel = "KinectForEllai", Payload = bodyMessages };
-
-            JavaScriptSerializer js = new JavaScriptSerializer();
-            String data = js.Serialize(message).ToLower();
-
+            String data = WebSocketHandler.Send(bodyMessages);
             Console.WriteLine(data);
-            WebSocket.Send(data);
         }
 
 
---
+++ src/KinectForEllai/KinectForEllai/WebSocketHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using WebSocketSharp;
+
+namespace KinectForEllai
+{
+    class WebSocketHandler
+    {
+        private WebSocket _webSocket;
+        private String _channel;
+        private readonly JavaScriptSerializer _js = new JavaScriptSerializer();
+
+        public WebSocketHandler(String host, String channel)
+        {
+            this._channel = channel;
+            _webSocket = new WebSocket("ws://" + host);
+
+            Connect();
+        }
+
+        public String Send(object obj)
+        {
+            var message = new { channel = this._channel, payload = obj };
+
+            String data = _js.Serialize(message);
+            _webSocket.Send(data);
+            return data;
+        }
+
+        private void Connect()
+        {
+            _webSocket.Connect();
+        }
+
+    }
+}
