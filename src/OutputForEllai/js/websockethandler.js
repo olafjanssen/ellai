dc215d92a5f61e79c8d7b334ee86846dd0e983f5
Olaf Janssen <o.t.a.janssen@gmail.com>
2018-01-30 10:14:40 +0100

    OutputForEllai first commit with general WebsocketHandler

---
+++ src/OutputForEllai/js/websockethandler.js
@@ -0,0 +1,45 @@
+/**
+ * Websocket helper module
+ */
+var WebsocketHandler = function(host, channel) {
+        
+        var socket = new WebSocket("ws://" + host, "protocolOne");
+        
+        socket.onopen = function(event){
+          console.log("WebsocketHandler: Connected");  
+        };
+        
+        socket.onerror = function(event) {
+            console.log("WebsocketHandler: ERROR ", event);
+        }
+        
+        socket.onmessage = function(event) {
+            var message = JSON.parse(event.data);
+            
+            if (ret.onMessage){
+                ret.onMessage(message.channel, message.payload);
+            }
+        }
+        
+        function connect() {
+            
+        }
+        
+        function disconnect() {
+            socket.close();
+        }
+        
+        function send(payload) {
+            var message = {channel: channel, payload: payload};
+            socket.send(JSON.stringify(message));
+        }
+        
+        var ret = {
+            onMessage: null,
+            send: send,
+            disconnect: disconnect  
+        }
+        
+        return ret;
+    };
+    
---
+++ src/OutputForEllai/server.js
@@ -0,0 +1,35 @@
+-/**
+ * Server for testing Kinect output via WebSockets
+ *
+ * @author Olaf Janssen (olaf.janssen@fontys.nl)
+ */
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  
+  ws.on('message', function incoming(message) {
+      var msg = JSON.parse(message);
+      
+      if (msg.channel === 'kinectforellai') {
+          showKinectMessage(msg.payload);
+      }
+  });
+
+  ws.on('close', function close() {
+    console.log('disconnected');
+  });
+    
+  ws.on('error', () => console.log('errored'));
+
+});
+
+
+/**
+ * Show the actual message payload sent to the kinect channel
+ */
+function showKinectMessage(payload) {
+    console.log(payload);
+}
